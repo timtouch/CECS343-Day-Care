8b8b2e35b32025806d5f128ae4306537e0e1622d
Timothy Touch <chaosbounder@gmail.com>
2016-10-25 10:38:05 -0700

    Add endpoint to retrieve list of all students

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ var status = require('http-status');
 module.exports = function(wagner){
   var api = express.Router();
 
+  // Get a student given their first name
   api.get('/student/firstName/:firstName', wagner.invoke(function(Student) {
     return function(req, res) {
       Student.findOne({ firstName: req.params.firstName}, function( error, student ) {
@@ -17,7 +18,26 @@ module.exports = function(wagner){
             status(status.NOT_FOUND).
             json({ error: 'Not found' });
         }
-        res.json( { student: student})
+        res.json( { student: student })
+      });
+    };
+  }));
+
+  // Get list of all students from DB
+  api.get('/student', wagner.invoke(function(Student) {
+    return function(req, res) {
+      Student.find({}, function(error, students){
+        if (error) {
+          return res.
+            status(status.INTERNAL_SERVER_ERROR).
+            json({ error: error.toString() });
+        }
+        if (!students) {
+          return res.
+            status(status.NOT_FOUND).
+            json({ error: 'Not found' });
+        }
+        res.json( {students: students });
       });
     };
   }));
