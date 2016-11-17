4e2e0d5428fadad68e33e6ef829e4cab8f34bd88
Timothy Touch <chaosbounder@gmail.com>
2016-11-16 21:58:03 -0800

    Accessing Mongo from online server

--- server/models.js
+++ server/models.js
@@ -3,8 +3,13 @@ var _ = require('underscore');
 
 module.exports = function(wagner){
   mongoose.Promise = global.Promise;
+  //NOTE: username: timtouch password: KidsInTheCrib
+  var mongolabURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test';
 
-  mongoose.connect('mongodb://localhost:27017/test');
+  mongoose.connect(mongolabURI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
 
   var Student =
     mongoose.model('Student', require('./student'), 'students');
