060e08802aa8ca73b6337d2214189ecffc7ecd92
timtouch <chaosbounder@gmail.com>
2016-11-23 18:16:36 -0800

    Add more setup for passportjs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var wagner = require('wagner-core');
 var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
 var flash = require('connect-flash');
 
+var path = require('path');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -10,33 +13,59 @@ var session = require('express-session');
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 3000;
 
-require('./models')(wagner);
+require('./models/models')(wagner);
 
+
+// user schema/model
+var User = require('./models/user.js');
+//create instance of express
 var app = express();
 
-app.use(require('morgan')());
+//Define middleware
+app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//api routes
 app.use('/api/v1', require('./api')(wagner));
 
 app.use(session({
   secret: 'shhitsasecret',
-  resave: true,
-  saveUninitialized: true
+  resave: false,
+  saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//configure passport
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..client', 'index.html'));
+});
 
 // Serve up static HTML pages from the file system.
-// For instance, '/6-examples/hello-http.html' in
-// the browser will show the '../6-examples/hello-http.html'
-// file.
 app.use(express.static('./client', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
 
-require('./client/routes.js')(app, passport);
+// error handlers
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.end(JSON.stringify({
+    message: err.message,
+    error: {}
+  }));
+});
+
 app.listen(port, function(){
   console.log('App is running on http://localhost:' + port);
 });
