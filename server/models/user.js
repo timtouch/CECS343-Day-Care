03c6c12fd067106617b5631498f36380ece2ede6
Dennis <dennisvu.csulb@gmail.com>
2016-12-05 12:05:59 -0800

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
-
 var User = new Schema({
   username: String,
   password: String,
