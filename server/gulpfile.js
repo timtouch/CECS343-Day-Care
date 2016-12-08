ec639ed6f5fb8ae09fa4a39cf176ab6281c3e01d
Dennis <dennisvu.csulb@gmail.com>
2016-12-08 11:56:50 -0800

    Update gulpfile.js

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -6,10 +6,12 @@ gulp.task('test', function(){
   gulp.
   src('./test.js').
   pipe(mocha()).
+  //if tests fail
   on('error', function() {
     console.log('Tests failed!');
     error = true;
   }).
+  //if tests pass
   on('end', function() {
     if(!error) {
       console.log('Tests succeeded!');
